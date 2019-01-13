98ed78ba0542c8fd175eeb53e5e424a1af0c8d3f
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-01-13 21:34:27 +0000

    Removing sounds from makefile

--- common.mk
+++ common.mk
@@ -7,8 +7,5 @@ include vendor/themes/accents/accents.mk
 # Boot animation
 include vendor/themes/bootanimation/bootanimation.mk
 
-# Sounds
-include vendor/themes/sounds/sounds.mk
-
 # Themes
 include vendor/themes/uithemes/uithemes.mk
