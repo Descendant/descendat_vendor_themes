6de84cbe3f65491e43f57a4120e32878f3b3e20d
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-01-13 21:45:58 +0000

    System Icon Switcher themes

--- common.mk
+++ common.mk
@@ -7,5 +7,8 @@ include vendor/themes/accents/accents.mk
 # Boot animation
 include vendor/themes/bootanimation/bootanimation.mk
 
+# System Icon Switcher 
+include vendor/sysiconswitcher/sysiconswitcher.mk
+
 # Themes
 include vendor/themes/uithemes/uithemes.mk
