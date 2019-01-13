c6a1052f6e966cd31e2544c4bc46b8fefa982c94
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-01-13 21:03:45 +0000

    Refactor dirs and confs

--- common.mk
+++ common.mk
@@ -2,13 +2,13 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 
 # Accents
-include vendor/themes/accents.mk
+include vendor/themes/accents/accents.mk
 
 # Boot animation
-include vendor/themes/bootanimation.mk
+include vendor/themes/bootanimation/bootanimation.mk
 
 # Sounds
-include vendor/themes/sounds.mk
+include vendor/themes/sounds/sounds.mk
 
 # Themes
-include vendor/themes/themes.mk
+include vendor/themes/uithemes/uithemes.mk
