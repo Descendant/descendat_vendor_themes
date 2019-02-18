39a01b7cdaa0f14c3602e312f90f2760045beecb
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-18 15:54:57 +0000

    Overlay homepage background color for black theme

--- uithemes/uithemes.mk
+++ uithemes/uithemes.mk
@@ -1,15 +1,9 @@
 # Themes
 PRODUCT_PACKAGES += \
-#    GBoardDark \
     GBoardLight \
-#    SettingsDark \
-#    SettingsIntelligenceDark \
-#    SystemDark \
-#    SystemUiDark \
-#    WellbeingDark \
     SystemBlackTheme \
-    SettingsBlackTheme \
     SystemUIBlackTheme \
+    SettingsBlackTheme \
     ContactsThemeBlack \
     DialerThemeBlack \
     WellbeingBlackTheme 
